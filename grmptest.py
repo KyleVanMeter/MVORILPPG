cd1000617f8ad9b95956c34980a5f72bf9a81450
Kyle Van Meter <kvanmete76751@gmail.com>
2019-11-24 12:16:16 -0700

    grUMpy visualization added our polytope method

--- grmptest.py
+++ grmptest.py
@@ -1,9 +1,18 @@
 from coinor.grumpy.polyhedron2D import Polyhedron2D, Figure
+import numpy as np
+
+def nsphere_to_cartesian(r, arr):
+    a = np.concatenate((np.array([2*np.pi]), arr))
+    si = np.sin(a)
+    si[0] = 1
+    si = np.cumprod(si)
+    co = np.cos(a)
+    co = np.roll(co, -1)
+    return si*co*r
 
 def RandPolytope(n, m):
     import scipy.spatial as sp
     import random
-    import numpy as np
 
     # Hyperspheroid coordinates take the form
     # x_1 = r*cos(t_1)
@@ -15,8 +24,8 @@ def RandPolytope(n, m):
     arr = []
     for i in range(m):
         angles = [np.deg2rad(random.randint(1,360)) for a in range(n-1)]
-        #arr.append(nsphere_to_cartesian(r,angles))
-        arr.append([random.uniform(0.0,50.0) for a in range(n)])
+        arr.append(nsphere_to_cartesian(r,angles))
+        #arr.append([random.uniform(0.0,50.0) for a in range(n)])
 
     # For now we just have the point (0) as our guaranteed point
     arr.append(np.array([0 for a in range(n)]))
@@ -33,9 +42,9 @@ def RandPolytope(n, m):
 points = RandPolytope(2,9)
 rays = []
 c = [2, 5]
-opt = [7, 5.7]
+opt = [1, 1]
 loc = (opt[0]+0.1, opt[1]-0.1)
-obj_val = 42.5
+obj_val = 1
 
 p = Polyhedron2D(points = points, rays = rays)
 f = Figure()
@@ -44,6 +53,4 @@ f.set_xlim([p.xlim[0], p.xlim[1]+1])
 f.set_ylim([p.ylim[0], p.ylim[1]+2])
 f.add_line(c, obj_val, p.xlim + [0.2, 0.8], p.ylim + [0.2, 1.8], 
            linestyle = 'dashed', color = 'black', label = "Objective Function")
-f.add_point(opt, 0.04, 'red')
-f.add_text(loc, r'$x^* = %s$' % str(opt))
 f.show()
